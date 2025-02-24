3c9735930f79075a92e669f994efd1bc822998b2
akhilpgvr <akhilpgvr@gmail.com>
2025-02-24 22:09:23 +0530

    updated vite config

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,7 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
-// vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-    allowedHosts: 'all' // Allow all hosts for deployment
-  },
-  build: {
-    outDir: 'dist'
-  }
-});
-
+})
